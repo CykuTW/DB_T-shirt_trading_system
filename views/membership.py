c735c38d70c2985161a12b6726bcb7596a4c8871
CykuTW <rft.tsumiki@gmail.com>
2017-11-30 14:18:40 +0800

    feat(views/membership): modify token verifying

--- views/membership.py
+++ views/membership.py
@@ -14,10 +14,15 @@ blueprint = Blueprint('membership', __name__)
 class LoginView(MethodView):
 
     def get(self):
-        return render_template('membership/login.html')
+        token_name, token = utils.generate_token()
+        return render_template('membership/login.html', token=token)
 
     @validate(validators.membership.LoginValidator)
     def post(self):
+        token = request.form.get('token')
+        if not utils.verify_token(token):
+            abort(401)
+
         username = request.form['username']
         password = request.form['password']
         user = models.Member.query.filter_by(username=username).first()
@@ -43,15 +48,14 @@ class ProfileView(MethodView):
 class RegisterView(MethodView):
 
     def get(self):
-        token = utils.generate_token()
-        session['token'] = token
+        token_name, token = utils.generate_token()
         return render_template('membership/register.html', token=token)
 
     @validate(validators.membership.RegisterValidator)
     def post(self):
         token = request.form.get('token')
-        if not token or token != session['token']:
-            abort(400)
+        if not utils.verify_token(token):
+            abort(401)
 
         username = request.form.get('username')
         realname = request.form.get('realname')
@@ -60,7 +64,6 @@ class RegisterView(MethodView):
         birthday = request.form.get('birthday')
         phone = request.form.get('phone')
         password = request.form.get('password')
-        confirm_password = request.form.get('confirm_password')
 
         user = models.Member.query.filter_by(username=username).first()
         # duplicate username
@@ -78,7 +81,6 @@ class RegisterView(MethodView):
         models.db.session.add(user)
         models.db.session.commit()
         return '註冊成功'
-
 
 
 class LogoutView(MethodView):
