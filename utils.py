df8a55aa5a6160c534af60c31e66019c0e842afd
CykuTW <rft.tsumiki@gmail.com>
2017-12-01 09:41:51 +0800

    fix(utils): fix verify_token logic error

--- utils.py
+++ utils.py
@@ -7,20 +7,20 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from Crypto.Hash import SHA256
 from urllib.parse import urlparse, urljoin
+from typing import Union
 
 
 bcrypt = Bcrypt()
 
 login_manager = LoginManager()
 
-def is_safe_url(target):
+def is_safe_url(target) -> bool:
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and \
            ref_url.netloc == test_url.netloc
 
-
-def generate_token(token_name=None):
+def generate_token(token_name=None) -> Union[str, str]:
     seq = string.ascii_letters + string.digits
     token = request.remote_addr + ''.join(random.choices(seq, k=30))
     h = SHA256.new()
@@ -33,11 +33,18 @@ def generate_token(token_name=None):
     session[token_name + '_generated_time'] = datetime.datetime.now().timestamp()
     return token_name, token
 
-def verify_token(token, token_name=None):
+def verify_token(token, token_name=None) -> bool:
     if not token_name:
         token_name = 'token'
-    expired = datetime.timedelta(seconds=session[token_name + '_expired'])
-    generate_time = datetime.datetime.fromtimestamp(session[token_name + '_generated_time'])
+
+    expired = session.pop(token_name + '_expired')
+    timestamp = session.pop(token_name + '_generated_time')
+    if not expired or not timestamp:
+        return False
+
+    expired = datetime.timedelta(seconds=expired)
+    generate_time = datetime.datetime.fromtimestamp(timestamp)
+
     return token and \
-            token == session[token_name] and \
-            datetime.datetime.now() <= generate_time + expired
+           token == session.pop(token_name) and \
+           datetime.datetime.now() <= generate_time + expired
