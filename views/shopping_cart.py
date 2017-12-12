be788305e27220a98b5531b8ddc5f0541d8b555b
CykuTW <rft.tsumiki@gmail.com>
2017-12-12 10:05:50 +0800

    feat(views/shopping_cart): add basic function to test

--- views/shopping_cart.py
+++ views/shopping_cart.py
@@ -1,7 +1,8 @@
 import utils
 import models
 import pickle
-from flask import Blueprint, request, abort, render_template
+import blinker
+from flask import Blueprint, request, abort, render_template, request_started
 from flask.views import MethodView
 from flask_login import login_required, current_user
 
@@ -11,35 +12,52 @@ blueprint = Blueprint('shopping_cart', __name__)
 
 class ShoppingCartView(MethodView):
 
+    @login_required
     def get(self):
-        good = models.Good.query.filter_by(id=1).first() or abort(400)
-        utils.redis_store.set('a', pickle.dumps(good))
-        print(type(pickle.loads(utils.redis_store.get('a'))))
-        return ''
+        user = current_user
+        items = utils.redis_store.lrange('_{}'.format(user.id), 0, -1)
+        items = [pickle.loads(i) for i in items]
+        return str(items)
 
     @login_required
     def post(self):
         good_id = request.form['good_id']
         good = models.Good.query.filter_by(id=good_id).first() or abort(400)
         user = current_user
-        utils.redis_store.set(str(current_user.id), 1)
         utils.redis_store.lpush(
-            '_{}'.format(current_user.id), 
+            '_{}'.format(user.id), 
             pickle.dumps(good)
         )
+        return 'OK'
 
 
-# Set up redis keyspace notification callback
-def _redis_del_key_handler(message):
-    print('redie expire:', message.data)
+@blueprint.before_request
+@login_required
+def _before_request():
+    user = current_user
+    if not utils.redis_store.get(str(user.id)):
+        utils.redis_store.set(str(user.id), 1)
+        utils.redis_store.expire(str(user.id), 60*60) # 1 hour
+        utils.redis_store.delete('_{}'.format(user.id))
+        for item in user.shopping_cart:
+            utils.redis_store.lpush(
+                '_{}'.format(user.id),
+                pickle.dumps(item.good)
+            )
 
-_pubsub = None
-def init_app(app):
-    global _pubsub
+
+@request_started.connect_via(blinker.ANY) # ANY sender
+def _register_signal_handler(sender, **extra):
+    # Set up redis keyspace notification callback
+    def _redis_del_key_handler(message):
+        print('redis expire:', message.data)
+
     _pubsub = utils.redis_store.pubsub()
     _pubsub.psubscribe(**{
         '__keyevent@0__:expire': _redis_del_key_handler
     })
-    _pubsub.run_in_thread()
+    _pubsub.run_in_thread(sleep_time=1)
+    request_started.disconnect(_register_signal_handler)
+
 
 blueprint.add_url_rule('/', view_func=ShoppingCartView.as_view(ShoppingCartView.__name__))
